7de136ab62517b8ee1979907ae7a99bcfadc14bd
Tomislav <124046839+markotatomislav@users.noreply.github.com>
2023-08-02 22:47:04 +0200

    edit user

--- EcommAlgebra/Areas/Admin/Controllers/UserController.cs
+++ EcommAlgebra/Areas/Admin/Controllers/UserController.cs
@@ -8,17 +8,44 @@ namespace EcommAlgebra.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
-        ApplicationDbContext _context;  
+        ApplicationDbContext _context;
 
         public UserController(ApplicationDbContext context)
         {
-            _context = context; 
+            _context = context;
         }
         public IActionResult Index()
         {
             var users = _context.Users.ToList();
 
             return View(users);
+        }
+
+        public IActionResult Update(string id) {
+
+            var user = _context.Users.FirstOrDefault(user => user.Id == id);
+
+            if (user == null) return NotFound();
+            
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Update(ApplicationUser user) {
+
+            var existingUser = _context.Users.FirstOrDefault(dbUser => dbUser.Id == user.Id);
+
+            if(existingUser == null) return NotFound();
+
+            existingUser.FirstName = user.FirstName;
+            existingUser.LastName = user.LastName;
+            existingUser.Email = user.Email;
+            existingUser.Address = user.Address;    
+
+            _context.Users.Update(existingUser);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
